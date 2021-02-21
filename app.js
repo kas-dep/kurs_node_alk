a09aca70ec12beedb023605d9325a0e58215396c
Katarzyna Depczyńska <kasiadepczynskapl@gmail.com>
2021-02-21 12:19:19 +0100

    add port

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const functions = require("./functions");
 const path = require('path')
-const port = 3000;
+const port = process.env.PORT || 3000;
 const app = express();
 
 const x = 11;
